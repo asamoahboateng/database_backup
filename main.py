63a6f71e7cc28828d7d2344afdeee170628e176c
Kwame Asamoah Boateng <kasamoahboateng@gmail.com>
2020-09-26 14:13:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ dbUser = "database username"
 dbPass = "database password"
 dbName = "database name"
 
+dbHost = "127.0.0.1"
+dbUser = "root"
+dbPass = ""
+dbName = "esoft"
+
 mydb = mysql.connector.connect(
   host=dbHost,
   user=dbUser,
@@ -36,35 +41,30 @@ mycursor.execute("show tables")
 
 myresult = mycursor.fetchall()
 
-for x in myresult:
+for (x,) in myresult: #tuple unpacking
     
     data2 = []
-    file_name = './'+folder_name+'/'+x[0]+'.json'
-    f= open(file_name,'w')
+    file_name = './'+folder_name+'/'+x+'.json'
     
-    reponse = "\n|>> Fetching Data from Table "
-    print(reponse)
-    print("|-->> Table \t: {}".format(x[0]))
+    #reponse = "\n|>> Fetching Data from Table "
+    print("\n|>> Fetching Data from Table ")
+    print("|-->> Table \t: {}".format(x))
     
-    fetch_query = "select * from " + x[0]
+    fetch_query = "select * from " + x
     mycursor.execute(fetch_query)
-    headers = [i[0] for i in mycursor.description]
     
-    tableName = x[0]
+    headers = [i[0] for i in mycursor.description] #get column headers
     
-    #headCount = 0
-    myresult = mycursor.fetchall()
+   
+    myresult = mycursor.fetchall() #all records
     print("|-->> Entities \t: {}".format(len(myresult)))
     
-    headCount = 0
     for x in myresult:
 
         data1 = {}
         for xx in range(len(headers)):
             data1[headers[xx]] = str(x[xx])
             
-        headCount = headCount+1
-        
         data2.append(data1)
 
     
@@ -72,13 +72,15 @@ for x in myresult:
     
     with open(file_name,'w+') as outfile:
         json.dump(fileData, outfile, sort_keys=True, indent=4)
+    
 
 #zipping folder
 zipfoldername = folder_name+'_'+ today.strftime("%d_%m_%Y")
 print('|>> Zipping Folder')
 shutil.make_archive(zipfoldername, 'zip', './'+folder_name)
 
-#deleting folder
+
+#time.sleep(5)
 print('|>> Deleting Folder')
 shutil.rmtree('./'+folder_name, ignore_errors=True)
 
